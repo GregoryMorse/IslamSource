50ccae62cfac9e56ca95d64b3e6383ef129ccba0
GregoryMorse <gregory.morse@live.com>
2014-11-08 20:41:11 +0000

    Working on fixing up Warsh text

--- IslamSource/clsWarshQuran.vb
+++ IslamSource/clsWarshQuran.vb
@@ -1,4 +1,91 @@
 ﻿Public Class clsWarshQuran
+    Shared TimesNewRomanPSMT As New Dictionary(Of String, Integer()) From {
+{"0", New Integer() {&H660}},
+{"1", New Integer() {&H661}},
+{"2", New Integer() {&H662}},
+{"3", New Integer() {&H663}},
+{"4", New Integer() {&H664}},
+{"5", New Integer() {&H665}},
+{"6", New Integer() {&H666}},
+{"7", New Integer() {&H667}},
+{"8", New Integer() {&H668}},
+{"9", New Integer() {&H669}},
+{"A", New Integer() {&H627}}}
+    Shared ProfessionalNaskhSupplement1Variants As New Dictionary(Of String, Integer()) From {
+{"", New Integer() {&HF22A}}}
+    Shared ProfessionalNaskhSupplements4Kashidahs As New Dictionary(Of String, Integer()) From {
+{"A", New Integer() {&H5D0, &HF242}},
+{"b", New Integer() {&HF249}},
+{"H", New Integer() {&HF25A}},
+{"r", New Integer() {&HF269, &HF26A}},
+{"s", New Integer() {&H6D, &H6F}},
+{"$", New Integer() {&HF272, &HF274}},
+{"S", New Integer() {&HF276}},
+{"lm", New Integer() {&HF29D}},
+{"E", New Integer() {&HF2A7, &HF2A9}},
+{"f", New Integer() {&HF2AF, &HF2B1}},
+{"l", New Integer() {&HF2BB}},
+{"n", New Integer() {&HF2C3, &HF2C5}},
+{"h", New Integer() {&HC7}},
+{"w", New Integer() {&HF2CB, &H10F}},
+{"y", New Integer() {&HF2CD}},
+{"p", New Integer() {&HF2D2}},
+{"", New Integer() {&H105, &H106, &H107, &H109, &H10F}}
+} '109 + {105=b/n, 106=t/y, 107=v}, 10F + {105=f, 106=q}
+    Shared Hamd2 As New Dictionary(Of String, Integer()) From {
+    {"<", New Integer() {&H3, &H74}},
+    {"i", New Integer() {&H7}},
+    {"laA", New Integer() {&HD, &H1E, &H6C, &H6F, &H71}},
+    {"l~uA", New Integer() {&H19}},
+    {"li>a", New Integer() {&H1A}},
+    {"luA", New Integer() {&H1D}},
+    {"}i", New Integer() {&H22, &H23, &H79}},
+    {"'", New Integer() {&H24, &HDB}},
+    {"Yo", New Integer() {&H25}},
+    {"}a", New Integer() {&H27, &H81, &H221E}},
+    {"|", New Integer() {&H4E}},
+    {"liA", New Integer() {&H50, &H7E}},
+    {"&a", New Integer() {&H51}},
+    {"w", New Integer() {&H53, &H55}},
+    {"LFA", New Integer() {&H5A, &H5B, &H5C, &H5D}},
+    {"La[A", New Integer() {&H60, &H61}},
+    {"L~a[A", New Integer() {&H62}},
+    {"L~FA", New Integer() {&H65, &H67, &H68}},
+    {"L~aA", New Integer() {&H6B}},
+    {"w^", New Integer() {&H70}},
+    {">a", New Integer() {&H75, &H77, &HDC, &HDD, &HDE}},
+    {"<i", New Integer() {&H76, &H7F}},
+    {"_", New Integer() {&H7B}},
+    {"la", New Integer() {&H7D}},
+    {"l", New Integer() {&H82}},
+    {"`", New Integer() {&H97, &H98, &HA2}},
+    {".", New Integer() {&H9B}},
+    {"`^", New Integer() {&HA0}},
+    {"-", New Integer() {&HA5}}
+    }
+
+    Shared MSHQuraan1 As New Dictionary(Of String, Integer()) From {
+    {"L", New Integer() {&H2203}},
+    {"Q", New Integer() {&H25}},
+    {"C", New Integer() {&H26}},
+    {"M", New Integer() {&H220B}},
+    {"I", New Integer() {&H2217}},
+    {"^", New Integer() {&H3A, &H3B}},
+    {"]", New Integer() {&H3E}},
+    {"", New Integer() {&H3A7, &H2206}},
+    {"!", New Integer() {&H3A6}},
+    {"`", New Integer() {&H397, &H3C5}},
+    {"#", New Integer() {&H39A}},
+    {"_", New Integer() {&H3A4}},
+    {"i", New Integer() {&H3B3}},
+    {"l", New Integer() {&HF081}},
+    {"A", New Integer() {&H201A}},
+    {"H", New Integer() {&H2014}},
+    {"(", New Integer() {&H2248}},
+    {")", New Integer() {&H2026}},
+    {",", New Integer() {&H2287}},
+    {"X", New Integer() {&H222B}}
+    }
     Shared Arr1 As New Dictionary(Of String, Integer()) From {
 {" ", New Integer() {&H20}},
 {"llh", New Integer() {&H21}},
@@ -33,7 +120,7 @@
     {"k", New Integer() {&H2E, &H30, &H31, &H32, &H33, &H34, &H35, &H36, &H37, &H38}},
     {"l", New Integer() {&H39, &H3A, &H3B, &H3C, &H3D, &H3E, &H3F, &H2245, &H391}},
     {"m", New Integer() {&H392, &H3A7, &H2206, &H395, &H393, &H397, &H399, &H3D1, &H39A, &H39B, &H39C, &H39D, &H39F, &H3A0, &H398}},
-    {"n", New Integer() {&H3A1, &H3A3, &H3A4, &H3A5, &H3C2, &H2126, &H39E, &H3A8, &H396, &H2234, &H5D, &H22A5, &H5F, &H23AF, &H3B2, &H3C7, &H2205, &H239D, &HF0, &H239E, &H23A5, &H23A6}},
+    {"n", New Integer() {&H3A1, &H3A3, &H3A4, &H3A5, &H3C2, &H2126, &H39E, &H3A8, &H396, &H2234, &H5D, &H22A5, &H5F, &H3B2, &H3C7, &H2205, &H239D, &HF0, &H239E, &H23A5, &H23A6, &H23AF}},
     {"h", New Integer() {&H3B4, &H3B5, &H3C6, &H3B3, &H3B7, &H3B9, &H3D5, &H3BA, &H3BB, &HB5, &H3BD}},
     {"p", New Integer() {&H3BF, &H3C0}},
     {"w", New Integer() {&H3B8, &H3C1}},
@@ -41,7 +128,7 @@
     {"lA", New Integer() {&H3C9, &H3BE}},
     {"l>", New Integer() {&H3C8, &H7B, &H7C, &H7D, &H223C}},
     {"y", New Integer() {&H192, &H201E, &H2020, &H2021, &H2C6, &H2030, &H160, &H2039, &HF08D, &HF08E, &HF08F, &HF090, &H23A7, &H23AA, &H23AB}},
-    {"Y", New Integer() {&H2018, &H2019, &H201C, &H201D, &HA9, &HAC, &H23A9, &H2321}},
+    {"Y", New Integer() {&HFF, &H2018, &H2019, &H201C, &H201D, &HA9, &HAC, &H23A9, &H2321}},
     {"}", New Integer() {&H2014, &H2DC, &H203A, &HF09D, &H2044, &H221E, &H2666}},
     {"#", New Integer() {&H2194, &H23AE}},
     {"X", New Integer() {&H2190, &H2033, &H2265, &HD7, &H221D}},
@@ -52,7 +139,7 @@
     {"`^", New Integer() {&H211C, &H2118}},
     {"_", New Integer() {&H2297}},
     {"=", New Integer() {&H2229}},
-    {"", New Integer() {&H222A, &HFF}},
+    {"", New Integer() {&H222A}},
     {"0", New Integer() {&H2283}},
     {"1", New Integer() {&H2287}},
     {"2", New Integer() {&H2284}},
@@ -70,11 +157,12 @@
     {"D", New Integer() {&H21D3}},
     {"(", New Integer() {&H2329}},
     {"9", New Integer() {&HAE}},
+    {")", New Integer() {&HAE}},
     {"'", New Integer() {&H2122}},
     {"t", New Integer() {&H23A2, &H222B, &H23A0}},
     {"b", New Integer() {&H2320, &H23A4}},
     {"v", New Integer() {&H239F}}
-    } '{")", New Integer() {&HAE}}, redundant but paranthesis only used if first character for soorah marker
+    }
     Shared Arr3 As New Dictionary(Of String, Integer()) From {
             {"l", New Integer() {&H21, &H23, &H51}},
             {"d", New Integer() {&H24}},
@@ -127,10 +215,9 @@
     {"^", New Integer() {&H21, &HF0AF}},
     {"`^", New Integer() {&H23}},
     {"{", New Integer() {&H24}},
-    {"o", New Integer() {&H26, &H27, &H28}},
+    {"@", New Integer() {&H26, &H27, &H28}},
     {"[", New Integer() {&H2D, &H2E, &H2F}},
     {"a+", New Integer() {&H30}},
-    {"-", New Integer() {&H31}},
     {"`", New Integer() {&H32, &H33, &H34}},
     {"u[", New Integer() {&H36, &H37, &H38, &H39}},
     {"~u^", New Integer() {&H3A, &H3B, &H3C}},
@@ -158,7 +245,7 @@
     {"aN", New Integer() {&H64}},
     {"i{", New Integer() {&H65}},
     {"ao", New Integer() {&H68}},
-    {"a", New Integer() {&H6D, &H6E, &H6F, &H70, &H71, &H72, &H73, &H74, &H75, &H76, &H77, &H78, &H79, &H7A, &H7B, &H7C, &H7D, &H7E, &HF080, &HF08D, &H161, &H178}},
+    {"a", New Integer() {&H31, &H6D, &H6E, &H6F, &H70, &H71, &H72, &H73, &H74, &H75, &H76, &H77, &H78, &H79, &H7A, &H7B, &H7C, &H7D, &H7E, &HF080, &HF08D, &H161, &H178}},
     {"~a", New Integer() {&H153, &HF09E}},
     {"I", New Integer() {&HA0}},
     {"O", New Integer() {&HA1}},
@@ -169,9 +256,8 @@
     {"~i~ai", New Integer() {&HB0}}
     }
     Shared Arr7 As New Dictionary(Of String, Integer()) From {
-    {"-", New Integer() {&H31}},
-    {"a", New Integer() {&H76, &H79, &H7D, &HF08D}}
-    }
+    {"a", New Integer() {&H31, &H76, &H79, &H7D, &HF08D}}
+    } '&H31 is an imalee fatha
     Class CompareChar
         Implements IComparer(Of ArrayList)
         Public Function Compare(x As ArrayList, y As ArrayList) As Integer Implements IComparer(Of ArrayList).Compare
@@ -219,6 +305,7 @@
                 Dim Segment As iTextSharp.text.pdf.parser.LineSegment = renderInfo.GetCharacterRenderInfos(Count).GetBaseline()
                 Chunks.Add(New Chunk(renderInfo.GetCharacterRenderInfos(Count).GetText(), renderInfo.GetFont().PostscriptFontName, Segment.GetStartPoint(), Segment.GetEndPoint()))
             Next
+            'Hamd2 must be read out of content stream directly as the encoding is non-standard
         End Sub
 
         Public Function GetResultantText() As String Implements iTextSharp.text.pdf.parser.ITextExtractionStrategy.GetResultantText
@@ -273,13 +360,27 @@
                     CurDict = Arr5
                 ElseIf Chunks(Count).FontName = "BAMDFB+HQPB7" Then
                     CurDict = Arr7
+                    'ElseIf Chunks(Count).FontName = "BAMBNO+DecoType-ProfessionalNaskhSupplement4Kashidahs" Then
+                    '    CurDict = ProfessionalNaskhSupplements4Kashidahs
+                    'ElseIf Chunks(Count).FontName = "BAMCPB+DecoType-ProfessionalNaskhSupplement1Variants" Then
+                    '    CurDict = ProfessionalNaskhSupplement1Variants
+                ElseIf Chunks(Count).FontName = "BAMCAO+TimesNewRomanPSMT" Then
+                    CurDict = TimesNewRomanPSMT
+                ElseIf Chunks(Count).FontName = "BAMDAC+Hamd2" Then
+                    CurDict = Hamd2
+                ElseIf Chunks(Count).FontName = "BAPFCA+MSH-Quraan1" Then
+                    CurDict = MSHQuraan1
                 ElseIf Chunks(Count).FontName = "BAMCBO+TimesNewRomanPSMT" Then
                     If Str.Length <> 0 AndAlso Str.Chars(Str.Length - 1) <> " "c Then Str += " "
-                ElseIf Chunks(Count).FontName = "TraditionalArabic" Then
+                    'ElseIf Chunks(Count).FontName = "TraditionalArabic" Then
                 End If
                 If Not CurDict Is Nothing Then
                     For Each KeyValue As Collections.Generic.KeyValuePair(Of String, Integer()) In CurDict
-                        If Array.IndexOf(KeyValue.Value, System.Text.Encoding.Unicode.GetBytes(Chunks(Count).Str)(0) + 256 * System.Text.Encoding.Unicode.GetBytes(Chunks(Count).Str)(1)) <> -1 Then
+                        Dim Match As Integer = Array.IndexOf(KeyValue.Value, System.Text.Encoding.Unicode.GetBytes(Chunks(Count).Str)(0) + 256 * System.Text.Encoding.Unicode.GetBytes(Chunks(Count).Str)(1))
+                        If Match <> -1 Then
+                            'redundant value corrections
+                            If KeyValue.Value(Match) = &H23AF And KeyValue.Key = "n" And Str.Length <> 0 AndAlso Str.Chars(Str.Length - 1) = "i" Then Continue For
+                            If KeyValue.Value(Match) = &HAE And KeyValue.Key = "9" And Str.Length = 0 Then Continue For
                             If Str.Length <> 0 AndAlso Str.Chars(Str.Length - 1) = "A" AndAlso KeyValue.Key = "{" Then
                                 Str = Str.Remove(Str.Length - 1) + KeyValue.Key
                             ElseIf KeyValue.Key = "i~ai" Or KeyValue.Key = "~i~ai" Then
@@ -291,7 +392,7 @@
                                 Str = Str.Insert(Str.Length - 1, KeyValue.Key.Chars(If(KeyValue.Key.Chars(0) = "~", 2, 1)) + KeyValue.Key.Chars(If(KeyValue.Key.Chars(0) = "~", 3, 2)))
                                 Str += KeyValue.Key.Chars(If(KeyValue.Key.Chars(0) = "~", 4, 3))
                                 'End If
-                            ElseIf KeyValue.Key <> String.Empty AndAlso KeyValue.Key.Length >= If(KeyValue.Key.Chars(0) = "~", 3, 2) AndAlso ("aiuo".IndexOf(KeyValue.Key.Chars(1)) <> -1 And "aiuo".IndexOf(KeyValue.Key.Chars(If(KeyValue.Key.Chars(0) = "~", 2, 0))) <> -1) Then
+                            ElseIf KeyValue.Key <> String.Empty AndAlso KeyValue.Key.Length >= If(KeyValue.Key.Chars(0) = "~", 3, 2) AndAlso ("aiuo^".IndexOf(KeyValue.Key.Chars(1)) <> -1 And "aiuo^".IndexOf(KeyValue.Key.Chars(If(KeyValue.Key.Chars(0) = "~", 2, 0))) <> -1) Then
                                 '"oa", "ia", "oi", "iu", "aa", "~ia", "ai", "~iu", "ii", "au", "~aa", "~ii", "~au", "ao", "~ai"
                                 'If Str.Length > 2 AndAlso Str.Substring(Str.Length - 3) = "llh" Then
                                 Str = Str.Insert(Str.Length - 1, KeyValue.Key.Chars(0))
@@ -300,16 +401,41 @@
                                 End If
                                 Str += KeyValue.Key.Chars(If(KeyValue.Key.Chars(0) = "~", 2, 1))
                                 'End If
+                            ElseIf KeyValue.Key = "~a" Then
+                                If Str.Length <> 0 AndAlso Str.Chars(Str.Length - 1) = "A" Then
+                                    Str = Str.Insert(Str.Length - 1, KeyValue.Key)
+                                Else
+                                    Str += KeyValue.Key
+                                End If
+                            ElseIf KeyValue.Key = "a" Then
+                                If Str.Length <> 0 AndAlso Str.Chars(Str.Length - 1) = "A" Then
+                                    Str = Str.Insert(Str.Length - 1, KeyValue.Key)
+                                ElseIf Str.Length <> 0 AndAlso Str.Chars(Str.Length - 1) = "a" Then
+                                Else
+                                    Str += KeyValue.Key
+                                End If
+                            ElseIf KeyValue.Key = "i" Then
+                                If Str.Length <> 0 AndAlso Str.Chars(Str.Length - 1) = "i" Then
+                                Else
+                                    Str += KeyValue.Key
+                                End If
+                            ElseIf KeyValue.Key = "~" Then
+                                If Str.Length <> 0 AndAlso Str.Chars(Str.Length - 1) = "A" Then
+                                    Str = Str.Insert(Str.Length - 1, KeyValue.Key)
+                                ElseIf Str.Length <> 0 AndAlso (Str.Chars(Str.Length - 1) = "a" Or Str.Chars(Str.Length - 1) = "i") Then
+                                    Str = Str.Insert(Str.Length - 1, KeyValue.Key)
+                                ElseIf Str.Length <> 0 AndAlso Str.Chars(Str.Length - 1) = "~" Then
+                                Else
+                                    Str += KeyValue.Key
+                                End If
                             ElseIf KeyValue.Key = "=" Then
                                 If Str.Substring(Str.LastIndexOf(" "c) + 1) = "1" Then
                                     If Chapter <> 0 Then QStr += "  </sura>" + vbCrLf
                                     Chapter += 1
                                     Dim Idx As Integer = Str.IndexOf(" (")
-                                    If Idx = -1 Then Idx = Str.IndexOf(" madaaniy~ap")
-                                    If Idx = -1 Then Idx = Str.IndexOf(" maa_ki~iy~apN")
-                                    If Idx = -1 Then Idx = Str.IndexOf(" maa_kii~y~apN")
-                                    If Idx = -1 Then Idx = Str.IndexOf(" maa_k~iiy~apN")
-                                    QStr += "  <sura index=""" + CStr(Chapter) + """ name=""" + IslamMetadata.HTTPCoding.XmlEncode(Str.Substring(0, Idx).Replace("suwrapu ", String.Empty).Replace("9 ", String.Empty)) + """>" + vbCrLf
+                                    If Idx = -1 Then Idx = Str.IndexOf(" madaniy~ap")
+                                    If Idx = -1 Then Idx = Str.IndexOf(" ma_k~iy~apN")
+                                    QStr += "  <sura index=""" + CStr(Chapter) + """ name=""" + IslamMetadata.HTTPCoding.XmlEncode(Str.Substring(0, Idx).Replace("suwrapu ", String.Empty).Replace(") ", String.Empty)) + """>" + vbCrLf
                                 End If
                                 Dim Index As Integer = Str.IndexOf("bisomi {ll~ahi {lr~aHoma`ni {lr~aHiymi ")
                                 If Index = -1 Then
@@ -324,10 +450,10 @@
                                     QStr += "    <aya index=""" + StrReverse(Str.Substring(Str.LastIndexOf(" "c) + 1)) + """ text=""" + IslamMetadata.HTTPCoding.XmlEncode(Str.Substring(0, Str.LastIndexOf(" "c))) + """ />" + vbCrLf
                                 End If
                                 Str = String.Empty
-                                Else
-                                    Str += KeyValue.Key
-                                End If
-                                Exit For
+                            Else
+                                Str += KeyValue.Key
+                            End If
+                            Exit For
                         End If
                     Next
                 End If
