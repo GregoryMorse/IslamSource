eac67b4ee38ff5d31c80a2675189ce4d8fd97335
GregoryMorse <gregory.morse@live.com>
2014-12-20 23:39:09 +0100

    Minor fix for packaging

--- PackageWP.bat
+++ PackageWP.bat
@@ -12,7 +12,7 @@ IF NOT "%1"=="" (
 svn checkout http://plugins.svn.wordpress.org/islamsource/trunk/
 "%ProgramFiles%\7-Zip\7z.exe" x -otrunk IslamSourceWP.zip
 move /Y trunk\IslamSource\*.* trunk
-for /D %%F in (trunk\IslamSource\*) do if EXIST %%F (move /Y %%F\*.* trunk\%%~nxF && rmdir /s /q %%F) else (move /Y %%F trunk)
+for /D %%F in (trunk\IslamSource\*) do if EXIST trunk\%%~nxF (move /Y %%F\*.* trunk\%%~nxF && rmdir /s /q %%F) else (move /Y %%F trunk)
 rmdir /s /q trunk\IslamSource
 svn add trunk\*
 for /D %%F in (trunk\*) do svn add %%F\*
