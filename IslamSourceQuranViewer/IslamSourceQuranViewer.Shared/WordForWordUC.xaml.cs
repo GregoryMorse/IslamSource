38684e256d85cda9889b05b3257b11ef5e48f16e
Gregory Morse <gregory.morse@live.com>
2016-02-13 16:47:37 +0100

    Added many languages with store compatibility testing and begin fixing transliteration crashing or other issues

--- IslamSourceQuranViewer/IslamSourceQuranViewer.Shared/WordForWordUC.xaml.cs
+++ IslamSourceQuranViewer/IslamSourceQuranViewer.Shared/WordForWordUC.xaml.cs
@@ -30,6 +30,7 @@ namespace IslamSourceQuranViewer
     {
         public WordForWordUC()
         {
+            UIChanger = new MyUIChanger();
             this.InitializeComponent();
         }
 
@@ -42,6 +43,7 @@ namespace IslamSourceQuranViewer
             this.ViewModel = new MyRenderModel(IslamMetadata.TanzilReader.GetRenderedQuranText(XMLRender.ArabicData.TranslitScheme.RuleBased, String.Empty, String.Empty, Division.ToString(), Selection.ToString(), String.Empty, "0", "1").Items);
             MainControl.ItemsSource = this.ViewModel.RenderItems;
         }
+        public MyUIChanger UIChanger { get; set; }
         public MyRenderModel ViewModel { get; set; }
         private async void RenderPngs_Click(object sender, RoutedEventArgs e)
         {
@@ -66,6 +68,47 @@ namespace IslamSourceQuranViewer
 
         private int Division;
         private int Selection;
+
+        private void ZoomIn_Click(object sender, RoutedEventArgs e)
+        {
+            UIChanger.AllFontSize += 1.0;
+        }
+
+        private void ZoomOut_Click(object sender, RoutedEventArgs e)
+        {
+            UIChanger.AllFontSize -= 1.0;
+        }
+
+        private void Default_Click(object sender, RoutedEventArgs e)
+        {
+            UIChanger.AllFontSize = 20.0;
+        }
+    }
+    public class MyUIChanger : INotifyPropertyChanged
+    {
+        public MyUIChanger()
+        {
+            _AllFontSize = 20.0;
+        }
+        private double _AllFontSize;
+        public double AllFontSize
+        {
+            get
+            {
+                return _AllFontSize;
+            }
+            set
+            {
+                _AllFontSize = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("AllFontSize"));
+            }
+        }
+        
+        #region Implementation of INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
     }
     public class MyRenderModel
     {
@@ -80,13 +123,15 @@ namespace IslamSourceQuranViewer
     {
         public MyRenderItem(XMLRender.RenderArray.RenderItem RendItem)
         {
-            Items = System.Linq.Enumerable.Select(RendItem.TextItems, (Arr) => (Arr.Text.GetType() == typeof(List<XMLRender.RenderArray.RenderItem>)) ? (object)new MyRenderModel((List<XMLRender.RenderArray.RenderItem>)Arr.Text) : (Arr.Text.GetType() == typeof(string) ? (object)new MyChildRenderItem { ItemText = (string)Arr.Text } : null)).Where(Arr => Arr != null);
+            Items = System.Linq.Enumerable.Select(RendItem.TextItems, (Arr) => (Arr.Text.GetType() == typeof(List<XMLRender.RenderArray.RenderItem>)) ? (object)new MyRenderModel((List<XMLRender.RenderArray.RenderItem>)Arr.Text) : (Arr.Text.GetType() == typeof(string) ? (object)new MyChildRenderItem { ItemText = (string)Arr.Text, Color = new SolidColorBrush(Windows.UI.Color.FromArgb(0xFF, XMLRender.Utility.ColorR(Arr.Clr), XMLRender.Utility.ColorG(Arr.Clr), XMLRender.Utility.ColorB(Arr.Clr))), Direction = Arr.DisplayClass == XMLRender.RenderArray.RenderDisplayClass.eArabic || Arr.DisplayClass == XMLRender.RenderArray.RenderDisplayClass.eRTL ? FlowDirection.RightToLeft : FlowDirection.LeftToRight } : null)).Where(Arr => Arr != null);
         }
         public IEnumerable<object> Items { get; set; }
     }
     public class MyChildRenderItem
     {
+        public SolidColorBrush Color { get; set;  }
         public string ItemText { get; set; }
+        public FlowDirection Direction { get; set; }
     }
     public class MyDataTemplateSelector : DataTemplateSelector
     {
