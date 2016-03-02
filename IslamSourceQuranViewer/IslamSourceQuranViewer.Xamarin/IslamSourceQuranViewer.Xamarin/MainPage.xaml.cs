a12ace9051c0d833e545e892ece7ee77100f105b
Gregory Morse <gregory.morse@live.com>
2016-03-02 01:47:32 +0100

    Android/iOS support coming in 4 stages: initial UI, navigation/app bar, wrap panel and final text and glyph calculations for full support

--- IslamSourceQuranViewer/IslamSourceQuranViewer.Xamarin/IslamSourceQuranViewer.Xamarin/MainPage.xaml.cs
+++ IslamSourceQuranViewer/IslamSourceQuranViewer.Xamarin/IslamSourceQuranViewer.Xamarin/MainPage.xaml.cs
@@ -1,10 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+
+public class AndroidiOSFileIO : XMLRender.PortableFileIO
+{
+    public /*async*/ string[] GetDirectoryFiles(string Path)
+    {
+        return System.IO.Directory.GetFiles(Path);
+    }
+    public /*async*/ Stream LoadStream(string FilePath)
+    {
+        return File.Open(FilePath, FileMode.Open, FileAccess.Read);
+    }
+    public /*async*/ void SaveStream(string FilePath, Stream Stream)
+    {
+        FileStream File = System.IO.File.Open(FilePath, FileMode.Create, FileAccess.Write);
+        File.Seek(0, SeekOrigin.Begin);
+        byte[] Bytes = new byte[4096];
+        int Read;
+        Stream.Seek(0, SeekOrigin.Begin);
+        Read = Stream.Read(Bytes, 0, Bytes.Length);
+        while (Read != 0)
+        {
+            File.Write(Bytes, 0, Read);
+            Read = Stream.Read(Bytes, 0, Bytes.Length);
+        }
+        File.Dispose();
+    }
+    public string CombinePath(params string[] Paths)
+    {
+        return System.IO.Path.Combine(Paths);
+    }
+    public /*async*/ void DeleteFile(string FilePath)
+    {
+        File.Delete(FilePath);
+    }
+    public /*async*/ bool PathExists(string Path)
+    {
+        return Directory.Exists(Path);
+    }
+    public /*async*/ void CreateDirectory(string Path)
+    {
+        Directory.CreateDirectory(Path);
+    }
+    public /*async*/ DateTime PathGetLastWriteTimeUtc(string Path)
+    {
+        return System.IO.File.GetLastWriteTimeUtc(Path);
+    }
+    public /*async*/ void PathSetLastWriteTimeUtc(string Path, DateTime Time)
+    {
+        System.IO.File.SetLastWriteTimeUtc(Path, Time);
+    }
+}
+public class AndroidiOSSettings : XMLRender.PortableSettings
+{
+    public string CacheDirectory
+    {
+        get
+        {
+            return Directory.GetCurrentDirectory();
+        }
+    }
+    public KeyValuePair<string, string[]>[] Resources
+    {
+        get
+        {
+            return (new List<KeyValuePair<string, string[]>>(System.Linq.Enumerable.Select("HostPageUtility=Acct,lang,unicode;IslamResources=Hadith,IslamInfo,IslamSource".Split(';'), Str => new KeyValuePair<string, string[]>(Str.Split('=')[0], Str.Split('=')[1].Split(','))))).ToArray();
+        }
+    }
+    public string[] FuncLibs
+    {
+        get
+        {
+            return new string[] { "IslamMetadata" };
+        }
+    }
+    public string GetTemplatePath()
+    {
+        return GetFilePath("metadata\\IslamSource.xml");
+    }
+    public string GetFilePath(string Path)
+    {
+        return System.IO.Path.Combine(Directory.GetCurrentDirectory(), Path);
+    }
+    public string GetUName(char Character)
+    {
+        return "";
+    }
+    public string GetResourceString(string baseName, string resourceKey)
+    {
+        return new System.Resources.ResourceManager(baseName + ".Resources", System.Reflection.Assembly.Load(baseName)).GetString(resourceKey, System.Threading.Thread.CurrentThread.CurrentUICulture);
+    }
+}
 
 namespace IslamSourceQuranViewer.Xam
 {
@@ -12,7 +105,147 @@ namespace IslamSourceQuranViewer.Xam
 	{
 		public MainPage ()
 		{
-			InitializeComponent ();
+            this.DataContext = this;
+            this.ViewModel = new MyTabViewModel();
+            UIChanger = new MyUIChanger();
+            InitializeComponent();
 		}
-	}
+        public MyUIChanger UIChanger { get; set; }
+        public MyTabViewModel ViewModel { get; set; }
+    }
+    //public static class ListFormattedTextBehavior
+    //{
+    //    #region FormattedText Attached dependency property
+
+    //    public static List<string> GetFormattedText(DependencyObject obj)
+    //    {
+    //        return (List<string>)obj.GetValue(FormattedTextProperty);
+    //    }
+
+    //    public static void SetFormattedText(DependencyObject obj, List<string> value)
+    //    {
+    //        obj.SetValue(FormattedTextProperty, value);
+    //    }
+
+    //    public static readonly DependencyProperty FormattedTextProperty =
+    //        DependencyProperty.RegisterAttached("FormattedText",
+    //        typeof(List<string>),
+    //        typeof(ListFormattedTextBehavior),
+    //        new PropertyMetadata(null, FormattedTextChanged));
+
+    //    private static void FormattedTextChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+    //    {
+    //        List<string> value = e.NewValue as List<string>;
+
+    //        TextBlock textBlock = sender as TextBlock;
+
+    //        if (textBlock != null)
+    //        {
+    //            textBlock.Inlines.Clear();
+    //            textBlock.Inlines.Add(new Windows.UI.Xaml.Documents.Run() { Text = value[0], FontFamily = new FontFamily(AppSettings.strOtherSelectedFont), FontSize = AppSettings.dOtherFontSize });
+    //            if (value.Count > 1)
+    //            {
+    //                textBlock.Inlines.Add(new Windows.UI.Xaml.Documents.Run() { Text = "(" + value[1] + ")", FontWeight = Windows.UI.Text.FontWeights.Bold, FlowDirection = FlowDirection.RightToLeft, FontFamily = new FontFamily(AppSettings.strSelectedFont), FontSize = AppSettings.dFontSize });
+    //                textBlock.Inlines.Add(new Windows.UI.Xaml.Documents.Run() { Text = value[2], FontFamily = new FontFamily(AppSettings.strOtherSelectedFont), FontSize = AppSettings.dOtherFontSize });
+    //            }
+    //        }
+    //    }
+
+    //    #endregion
+    //}
+    public class MyTabViewModel : INotifyPropertyChanged
+    {
+        public MyTabViewModel()
+        {
+        }
+        private IEnumerable<MyTabItem> _Items;
+        public IEnumerable<MyTabItem> Items
+        {
+            get
+            {
+                return _Items;
+            }
+            set
+            {
+                _Items = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Items"));
+            }
+        }
+
+        private IEnumerable<MyListItem> _ListItems;
+        public IEnumerable<MyListItem> ListItems
+        {
+            get
+            {
+                return _ListItems;
+            }
+            private set
+            {
+                _ListItems = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("ListItems"));
+            }
+        }
+
+        public MyListItem ListSelectedItem
+        {
+            get { return _selectedItem == null ? null : _selectedItem.SelectedItem; }
+            set
+            {
+                _selectedItem.SelectedItem = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("ListSelectedItem"));
+            }
+        }
+
+        private MyTabItem _selectedItem;
+
+        public MyTabItem SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                _selectedItem = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("SelectedItem"));
+                ListItems = _selectedItem.Items;
+                ListSelectedItem = ListItems.First();
+            }
+        }
+
+        #region Implementation of INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+
+    public class MyTabItem
+    {
+        public string Title { get; set; }
+        public int Index { get; set; }
+        private IEnumerable<MyListItem> _Items;
+        public IEnumerable<MyListItem> Items
+        {
+            get
+            {
+                if (_Items == null) { _Items = System.Linq.Enumerable.Select(IslamMetadata.TanzilReader.GetSelectionNames(Index.ToString(), XMLRender.ArabicData.TranslitScheme.RuleBased, String.Empty), (Arr, Idx) => new MyListItem { TextItems = new List<string>(((string)(Arr.Cast<object>()).First()).Split('(', ')')), Index = (int)(Arr.Cast<object>()).Last() }); }
+                return _Items;
+            }
+        }
+
+        private MyListItem _selectedItem;
+
+        public MyListItem SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                _selectedItem = value;
+            }
+        }
+    }
+
+    public class MyListItem
+    {
+        public List<string> TextItems { get; set; }
+        public int Index { get; set; }
+    }
 }
--- IslamSourceQuranViewer/IslamSourceQuranViewer.Xamarin/IslamSourceQuranViewer.Xamarin/WordForWordUC.xaml.cs
+++ IslamSourceQuranViewer/IslamSourceQuranViewer.Xamarin/IslamSourceQuranViewer.Xamarin/WordForWordUC.xaml.cs
@@ -8,6 +8,7 @@ using Xamarin.Forms;
 
 using System.Collections;
 using System.Collections.Specialized;
+using System.ComponentModel;
 
 namespace IslamSourceQuranViewer.Xam
 {
@@ -311,4 +312,334 @@ namespace IslamSourceQuranViewer.Xam
         }
     }
 
+    public class MyUIChanger : INotifyPropertyChanged
+    {
+        public MyUIChanger()
+        {
+        }
+        public double FontSize
+        {
+            get
+            {
+                return AppSettings.dFontSize;
+            }
+            set
+            {
+                AppSettings.dFontSize = value;
+                //if (_DWArabicFormat != null) _DWArabicFormat.Dispose();
+                //_DWArabicFormat = null;
+                PropertyChanged(this, new PropertyChangedEventArgs("FontSize"));
+            }
+        }
+        public string FontFamily
+        {
+            get
+            {
+                return AppSettings.strSelectedFont;
+            }
+        }
+        public double OtherFontSize
+        {
+            get
+            {
+                return AppSettings.dOtherFontSize;
+            }
+            set
+            {
+                AppSettings.dOtherFontSize = value;
+                //if (_DWNormalFormat != null) _DWNormalFormat.Dispose();
+                //_DWNormalFormat = null;
+                PropertyChanged(this, new PropertyChangedEventArgs("OtherFontSize"));
+            }
+        }
+        public string OtherFontFamily
+        {
+            get
+            {
+                return AppSettings.strOtherSelectedFont;
+            }
+        }
+        public static void CleanupDW()
+        {
+            //if (_DWNormalFormat != null) _DWNormalFormat.Dispose();
+            //_DWNormalFormat = null;
+            //if (_DWArabicFormat != null) _DWArabicFormat.Dispose();
+            //_DWArabicFormat = null;
+            //if (_DWFactory != null) _DWFactory.Dispose();
+            //_DWFactory = null;
+        }
+        //private static SharpDX.DirectWrite.Factory _DWFactory;
+        //public static SharpDX.DirectWrite.Factory DWFactory { get { if (_DWFactory == null) _DWFactory = new SharpDX.DirectWrite.Factory(); return _DWFactory; } }
+        //private static SharpDX.DirectWrite.TextFormat _DWArabicFormat;
+        //public static SharpDX.DirectWrite.TextFormat DWArabicFormat { get { if (_DWArabicFormat == null) _DWArabicFormat = new SharpDX.DirectWrite.TextFormat(MyUIChanger.DWFactory, AppSettings.strSelectedFont, (float)AppSettings.dFontSize); return _DWArabicFormat; } }
+        //private static SharpDX.DirectWrite.TextFormat _DWNormalFormat;
+        //public static SharpDX.DirectWrite.TextFormat DWNormalFormat { get { if (_DWNormalFormat == null) _DWNormalFormat = new SharpDX.DirectWrite.TextFormat(MyUIChanger.DWFactory, AppSettings.strOtherSelectedFont, (float)AppSettings.dOtherFontSize); return _DWNormalFormat; } }
+        private double _MaxWidth;
+        public double MaxWidth
+        {
+            get
+            {
+                return _MaxWidth;
+            }
+            set
+            {
+                _MaxWidth = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("MaxWidth"));
+            }
+        }
+
+        #region Implementation of INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+    public class MyRenderModel : INotifyPropertyChanged
+    {
+        public MyRenderModel(IEnumerable<MyRenderItem> NewRenderItems)
+        {
+            RenderItems = NewRenderItems;
+            MaxWidth = CalculateWidth();
+        }
+        private double _MaxWidth;
+        public double MaxWidth { get { return _MaxWidth; } set { _MaxWidth = value; if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("MaxWidth")); } }
+        private double CalculateWidth()
+        {
+            return RenderItems.Select((Item) => Item.MaxWidth).Sum();
+        }
+        private List<MyRenderItem> _RenderItems;
+        public IEnumerable<MyRenderItem> RenderItems { get { return _RenderItems; } set { _RenderItems = value.ToList(); if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("RenderItems")); } }
+        #region Implementation of INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+    public class MyRenderItem : INotifyPropertyChanged
+    {
+        public MyRenderItem(XMLRender.RenderArray.RenderItem RendItem)
+        {
+            Items = System.Linq.Enumerable.Select(RendItem.TextItems.GroupBy((MainItems) => (MainItems.DisplayClass == XMLRender.RenderArray.RenderDisplayClass.eArabic || MainItems.DisplayClass == XMLRender.RenderArray.RenderDisplayClass.eLTR || MainItems.DisplayClass == XMLRender.RenderArray.RenderDisplayClass.eRTL || MainItems.DisplayClass == XMLRender.RenderArray.RenderDisplayClass.eTransliteration) ? (object)MainItems.DisplayClass : (object)MainItems), (Arr) => (Arr.First().Text.GetType() == typeof(List<XMLRender.RenderArray.RenderItem>)) ? (object)new MyRenderModel(System.Linq.Enumerable.Select((List<XMLRender.RenderArray.RenderItem>)Arr.First().Text, (ArrRend) => new MyRenderItem((XMLRender.RenderArray.RenderItem)ArrRend))) : (Arr.First().Text.GetType() == typeof(string) ? (object)new MyChildRenderItem(System.Linq.Enumerable.Select(Arr, (ArrItem) => new MyChildRenderBlockItem() { ItemText = (string)ArrItem.Text, Clr = Windows.UI.Color.FromArgb(0xFF, XMLRender.Utility.ColorR(ArrItem.Clr), XMLRender.Utility.ColorG(ArrItem.Clr), XMLRender.Utility.ColorB(ArrItem.Clr)) }).ToList(), Arr.First().DisplayClass == XMLRender.RenderArray.RenderDisplayClass.eArabic, (Arr.First().DisplayClass == XMLRender.RenderArray.RenderDisplayClass.eArabic || Arr.First().DisplayClass == XMLRender.RenderArray.RenderDisplayClass.eRTL) ? FlowDirection.RightToLeft : FlowDirection.LeftToRight) : null)).Where(Arr => Arr != null);
+            MaxWidth = CalculateWidth();
+        }
+        public double MaxWidth { get; set; }
+        private double CalculateWidth()
+        {
+            if (Items.Count() == 0) return 0.0;
+            return Items.Select((Item) => Item.GetType() == typeof(MyChildRenderItem) ? ((MyChildRenderItem)Item).MaxWidth : ((MyRenderModel)Item).MaxWidth).Max();
+        }
+        private List<object> _Items;
+        public IEnumerable<object> Items { get { return _Items; } set { _Items = value.ToList(); if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("Items")); } }
+        #region Implementation of INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+    public class VirtualizingWrapPanelAdapter : INotifyPropertyChanged
+    {
+        private List<MyRenderModel> _RenderModels;
+        public List<MyRenderModel> RenderModels
+        {
+            get
+            {
+                return _RenderModels;
+            }
+            set
+            {
+                _RenderModels = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("RenderModels"));
+            }
+        }
+        public void RegroupRenderModels(double maxWidth)
+        {
+            if (_RenderModels != null) { RenderModels = GroupRenderModels(_RenderModels.SelectMany((Item) => Item.RenderItems).ToList(), maxWidth); }
+        }
+        public static List<MyRenderModel> GroupRenderModels(List<MyRenderItem> value, double maxWidth)
+        {
+            double width = 0.0;
+            int groupIndex = 0;
+            List<int[]> GroupIndexes = new List<int[]>();
+            value.FirstOrDefault((Item) => {
+                double itemWidth = Math.Min(Item.MaxWidth, maxWidth); if (width + itemWidth > maxWidth) { width = itemWidth; groupIndex++; } else { width += itemWidth; }
+                GroupIndexes.Add(new int[] { groupIndex, GroupIndexes.Count }); return false;
+            });
+            return GroupIndexes.GroupBy((Item) => Item[0], (Item) => value.ElementAt(Item[1])).Select((Item) => new MyRenderModel(Item)).ToList();
+        }
+        #region Implementation of INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+    //public static class FormattedTextBehavior
+    //{
+    //    #region FormattedText Attached dependency property
+
+    //    public static List<MyChildRenderBlockItem> GetFormattedText(DependencyObject obj)
+    //    {
+    //        return (List<MyChildRenderBlockItem>)obj.GetValue(FormattedTextProperty);
+    //    }
+
+    //    public static void SetFormattedText(DependencyObject obj, List<MyChildRenderBlockItem> value)
+    //    {
+    //        obj.SetValue(FormattedTextProperty, value);
+    //    }
+
+    //    public static readonly DependencyProperty FormattedTextProperty =
+    //        DependencyProperty.RegisterAttached("FormattedText",
+    //        typeof(List<MyChildRenderBlockItem>),
+    //        typeof(FormattedTextBehavior),
+    //        new PropertyMetadata(null, FormattedTextChanged));
+
+    //    private static void FormattedTextChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+    //    {
+    //        List<MyChildRenderBlockItem> value = e.NewValue as List<MyChildRenderBlockItem>;
+
+    //        TextBlock textBlock = sender as TextBlock;
+
+    //        if (textBlock != null)
+    //        {
+    //            textBlock.Inlines.Clear();
+    //            value.FirstOrDefault((it) => { textBlock.Inlines.Add(new Windows.UI.Xaml.Documents.Run() { Text = it.ItemText, Foreground = it.Color }); return false; });
+    //        }
+    //    }
+
+    //    #endregion
+    //}
+    public class MyChildRenderItem : INotifyPropertyChanged
+    {
+        private double _MaxWidth;
+        public double MaxWidth { get { return _MaxWidth; } set { _MaxWidth = value; if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("MaxWidth")); } }
+        //public static double CalculateWidth(string text, bool IsArabic, float maxWidth, float maxHeight)
+        //{
+        //    //SharpDX.DirectWrite.TextLayout layout = new SharpDX.DirectWrite.TextLayout(MyUIChanger.DWFactory, text, IsArabic ? MyUIChanger.DWArabicFormat : MyUIChanger.DWNormalFormat, maxWidth, maxHeight);
+        //    //double width = layout.Metrics.WidthIncludingTrailingWhitespace + layout.Metrics.Left;
+        //    //layout.Dispose();
+        //    //return width;
+        //}
+        //public double CalculateWidth()
+        //{
+        //    //5 margin on both sides
+        //    return 5 + 5 + 1 + 1 + CalculateWidth(string.Join(String.Empty, ItemRuns.Select((Item) => Item.ItemText)), IsArabic, (float)float.MaxValue, float.MaxValue);
+        //}
+        #region Implementation of INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+ 
+        //public static short[] GetWordDiacriticClusters(string Str, string useFont, float fontSize, bool IsRTL)
+        //{
+        //}
+        //public static Size GetWordDiacriticPositionsDWrite(string Str, string useFont, float fontSize, char[] Forms, bool IsRTL, ref float BaseLine, ref CharPosInfo[] Pos)
+        //{
+        //}
+
+        //public MyChildRenderItem(List<MyChildRenderBlockItem> NewItemRuns, bool NewIsArabic, FlowDirection NewDirection)
+        //{
+        //    IsArabic = NewIsArabic; //must be set before ItemRuns
+        //    Direction = NewDirection;
+        //    ItemRuns = NewItemRuns;
+        //    MaxWidth = CalculateWidth();
+        //}
+        List<MyChildRenderBlockItem> _ItemRuns;
+        public List<MyChildRenderBlockItem> ItemRuns
+        {
+            get { return _ItemRuns; }
+            set
+            {
+                if (IsArabic)
+                {
+                    char[] Forms = XMLRender.ArabicData.GetPresentationForms;
+                    //XMLRender.ArabicData.LigatureInfo[] ligs = XMLRender.ArabicData.GetLigatures(String.Join(String.Empty, System.Linq.Enumerable.Select(value, (Run) => Run.ItemText)), false, Forms);
+                    //short[] chpos = GetWordDiacriticClusters(String.Join(String.Empty, System.Linq.Enumerable.Select(value, (Run) => Run.ItemText)), AppSettings.strSelectedFont, (float)AppSettings.dFontSize, IsArabic);
+                    int pos = value[0].ItemText.Length;
+                    int count = 1;
+                    while (count < value.Count)
+                    {
+                        //if (value[count].ItemText.Length != 0 && pos != 0 && chpos[pos] == chpos[pos - 1])
+                        //{
+                        //    //if (!XMLRender.ArabicData.IsDiacritic(value[count].ItemText.First()) && value[count - 1].Clr != Windows.UI.Colors.Black) { value[count - 1].Clr = value[count].Clr; }
+                        //    if (chpos[pos] == chpos[pos + value[count].ItemText.Length - 1])
+                        //    {
+                        //        pos -= value[count - 1].ItemText.Length;
+                        //        value[count - 1].ItemText += value[count].ItemText; value.RemoveAt(count); count--;
+                        //    }
+                        //    else {
+                        //        int subcount = pos;
+                        //        while (subcount < pos + value[count].ItemText.Length && chpos[subcount] == chpos[subcount + 1]) { subcount++; }
+                        //        value[count - 1].ItemText += value[count].ItemText.Substring(0, subcount - pos + 1); value[count].ItemText = value[count].ItemText.Substring(subcount - pos + 1);
+                        //        pos += subcount - pos + 1;
+                        //    }
+                        //}
+                        //for (int subcount = 0; subcount < ligs.Length; subcount++) {
+                        //    //for indexes which are before, and after, find the maximum index in this group which is after
+                        //    if (pos > ligs[subcount].Indexes[0] && pos <= ligs[subcount].Indexes[ligs[subcount].Indexes.Length - 1]) {
+                        //        int ligcount;
+                        //        for (ligcount = ligs[subcount].Indexes.Length - 1; ligcount >= 0; ligcount--) {
+                        //            if (pos + value[count].ItemText.Length <= ligs[subcount].Indexes[ligcount]) {
+                        //                if (ligs[subcount].Indexes[ligcount] - pos == value[count].ItemText.Length) {
+                        //                    pos -= value[count - 1].ItemText.Length;
+                        //                    if (!value[count].ItemText.All((ch) => XMLRender.ArabicData.IsDiacritic(ch))) { value[count - 1].Clr = value[count].Clr; }
+                        //                    value[count - 1].ItemText += value[count].ItemText; value.RemoveAt(count); count--;
+                        //                } else {
+                        //                    value[count - 1].ItemText += value[count].ItemText.Substring(0, ligs[subcount].Indexes[ligcount] - pos); value[count].ItemText = value[count].ItemText.Substring(ligs[subcount].Indexes[ligcount] - pos);
+                        //                    pos += ligs[subcount].Indexes[ligcount] - pos;
+                        //                }
+                        //                break;
+                        //            }
+                        //        }
+                        //        if (ligcount != ligs[subcount].Indexes.Length) { break; }
+                        //    }
+                        //}
+                        pos += value[count].ItemText.Length;
+                        count++;
+                        //int charcount = 0;
+                        //while (charcount < value[count].ItemText.Length && XMLRender.ArabicData.IsDiacritic(value[count].ItemText[charcount])) { charcount++; }
+                        //if (charcount != 0)
+                        //{
+                        //    if (charcount == value[count].ItemText.Length)
+                        //    {
+                        //        value[count - 1].ItemText += value[count].ItemText; value.RemoveAt(count);
+                        //    }
+                        //    else { value[count - 1].ItemText += value[count].ItemText.Substring(0, charcount); value[count].ItemText = value[count].ItemText.Substring(charcount); count++; }
+                        //}
+                        //else { count++; }
+                    }
+                }
+                _ItemRuns = value;
+            }
+        }
+        //public FlowDirection Direction { get; set; }
+        public bool IsArabic { get; set; }
+    }
+    public class MyChildRenderBlockItem
+    {
+        public Windows.UI.Color Clr;
+        //public SolidColorBrush Color { get { return new SolidColorBrush(Clr); } }
+        public string ItemText { get; set; }
+    }
+    //public class MyDataTemplateSelector : DataTemplateSelector
+    //{
+    //    public DataTemplate MyTemplate { get; set; }
+
+    //    protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+    //    {
+    //        return MyTemplate;
+    //    }
+    //}
+    //public class NormalWordTemplateSelector : DataTemplateSelector
+    //{
+    //    public DataTemplate WordTemplate { get; set; }
+    //    public DataTemplate ArabicTemplate { get; set; }
+    //    public DataTemplate NormalTemplate { get; set; }
+
+    //    protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+    //    {
+    //        if (item.GetType() == typeof(MyRenderModel)) { return WordTemplate; }
+    //        return ((MyChildRenderItem)item).IsArabic ? ArabicTemplate : NormalTemplate;
+    //    }
+
+    //}
 }
